c01e7f47ea485428fd252504fcea82d66d81c54e
HIilLm <97495105+HIilLm@users.noreply.github.com>
2022-10-05 07:58:07 +0700

    first commit

---
+++ resources/views/dashboards/opreason/index_opreason.blade.php
@@ -0,0 +1,59 @@
+@extends('layouts.dashboard')
+
+@section('content')
+    <div class="page-content">
+        <div class="main-wrapper">
+
+            <div class="row">
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Title Page</h5>
+
+                            </div>
+                        </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+    @endsection
+    @section('js')
+        <script type="text/javascript" src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
+        <script type="text/javascript" src="https://cdn.datatables.net/1.11.3/js/dataTables.bootstrap4.min.js"></script>
+        <script>
+            $(document).ready(function() {
+                            var id;
+                            $('.editable').each(function() {
+                                id = $(this).attr('id');
+                                if (id != '') {
+                                    $('#' + id).summernote({
+                                        height: 120,
+                                    });
+                                }
+                            })
+                        });
+
+            var table = $('#logo-table').DataTable({});
+
+            table.on('row-reordered', function(e, diff, edit) {
+                setTimeout(() => {
+                    var obj_id = table.column(1).data().toArray();
+                    console.log(obj_id);
+                    $.ajax({
+                        type: "post",
+                        url: "{{ url('') }}/admin/project/position",
+                        dataType: "json",
+                        data: {
+                            id: obj_id,
+                            _token: '{{ csrf_token() }}'
+                        },
+                        success: function(response) {
+                            console.log(response);
+                        }
+                    });
+                }, 2000);
+            });
+        </script>
+    @endsection
